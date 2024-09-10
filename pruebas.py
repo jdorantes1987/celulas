90557ef5a54a178fecaa53281a762d237ba4f895
jdorantes1987 <dorantes.jackson@gmail.com>
2024-09-09 22:34:08 -0400

    Sobres entregados el 08-09-2024

--- pruebas.py
+++ pruebas.py
@@ -1,25 +1,4 @@
-
-import bcrypt
-# from gestion_user.control_usuarios import aut_user
+from scripts.oCelulas import DataCelulas
 
 
-txt = 'user002'
-pw = txt.encode('utf-8')
-sal = bcrypt.gensalt()
-
-encript = bcrypt.hashpw(pw, sal)
-
-# contrasenia_encript = bytes(data_user('JDORANTES')['passw'][0], 'utf-8')
-
-print(encript)
-
-# # result = bcrypt.checkpw(bytes('18329114', 'utf-8'), contrasenia_encript)
-
-# # print(result)
-
-# print(aut_user('JDORANTES', '18329114'))
-
-
-# from scripts.oCelulas import DataCelulas
-
-# print(DataCelulas().sobres_por_entregar())
+DataCelulas().celulas_activas_hist().to_excel('celulas_activas_hist.xlsx') 
