8dd80f3fb26365ca7d07b2d04e5d1487bf7c11eb
jdorantes1987 <165170008+jdorantes1987@users.noreply.github.com>
2025-03-03 13:23:11 -0400

    Se optimizo login

--- app.py
+++ app.py
@@ -5,31 +5,63 @@ from gestion_user.control_usuarios import aut_user
 from gestion_user.usuarios_roles import ClsUsuariosRoles
 
 
-st.set_page_config(page_title='Células', 
-                   layout="centered", 
-                   initial_sidebar_state="collapsed", 
-                   page_icon=':vs:')
+st.set_page_config(
+    page_title="Células",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    page_icon=":vs:",
+)
 
-MENU_INICIO = 'pages/page1.py'
+MENU_INICIO = "pages/page1.py"
+
 
 def roles():
     return ClsUsuariosRoles.roles()
 
-st.title("Datos del usuario")
-st.write("Ingrese sus datos.")
-username = st.text_input("usuario")
-password = st.text_input("clave", type="password")
 
-if st.button("Iniciar sesión", type="primary"):
-    if is_valid := aut_user(username, password):
+st.title("Inicio de sesión")
+
+
+def set_stage(i):
+    st.session_state.stage = i
+
+
+if "stage" not in st.session_state:
+    st.session_state.stage = 0
+
+
+def login(user, passw):
+    if aut_user(user=user, pw=passw):
+        set_stage(0)
         date = datetime.now()
-        st.cache_data.clear()
-        print(f"{date} Usuario {username} ha iniciado sesión.")
+        print(f"{date} Usuario {user} ha iniciado sesión.")
         st.session_state.logged_in = True
-        st.success("Sesión iniciada!")
+        st.success("Sesión iniciada exitosamente!")
+        st.cache_data.clear()
         sleep(0.5)
-        roles = roles()
         st.switch_page(MENU_INICIO)
-    else:
-        st.error("usuario o clave incorrecta")
-        
+        return True
+    return False
+
+
+if "usuario" not in st.session_state:
+    st.write("Por favor ingrese su usuario.")
+    # Si el usuario aún no ha sido ingresado
+    usuario = st.text_input("Usuario:")
+    if usuario and len(str.strip(usuario)) > 0:
+        st.session_state.usuario = usuario
+        st.rerun()
+else:
+    # Si el usuario ya ha sido ingresado, se oculta el input y se muestra el usuario ingresado
+    st.write(f"### Usuario ingresado: :red[{st.session_state.usuario}]")
+
+    # Pedir la contraseña
+    password = st.text_input("Ingresa tu contraseña:", type="password")
+    if st.button("Log in", type="primary"):
+        login(user=st.session_state.usuario, passw=password)
+
+    if password:
+        if not login(user=st.session_state.usuario, passw=password):
+            st.error("usuario o clave incorrecta")
+            sleep(1)
+            st.rerun()
