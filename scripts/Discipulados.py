fd72b3526a9844b590b1e59c791b093ed5b9295c
jdorantes1987 <165170008+jdorantes1987@users.noreply.github.com>
2025-07-10 19:10:25 -0400

    Actualización

--- scripts/Discipulados.py
+++ scripts/Discipulados.py
@@ -7,15 +7,3 @@ class Discipulados:
         Obtiene los códigos de la hoja de cálculo.
         """
         return self.oSheets.get_data_hoja(sheet_name="discipulados")
-
-
-if __name__ == "__main__":
-    from scripts.data_sheets import ManagerSheets
-
-    manager_sheets = ManagerSheets(
-        file_sheet_name="celulas",
-        spreadsheet_id="1GEtYGIQCucTTd6yVuC7dssMuJeYklbehJfWAC1UNkpE",
-        credentials_file="key.json",
-    )
-    oData = Discipulados(manager_sheets=manager_sheets)
-    print(oData.get_discipulados())
